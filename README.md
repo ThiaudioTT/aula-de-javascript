f27cb4c599491d96ee992e43676f9b4aa6467add
Thiago Araújo <thiago.sky25@gmail.com>
2021-07-04 12:52:12 -0300

    -

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # aula-de-javascript
-Aula de javascript apresentando os principais conceitos
+Aula de javascript apresentando os principais conceitos.
+
+Pode servir para eventuais consultas. Claro, não irá servir como um livro explicando tudo, mas irá servir "de passada".
---
+++ js/main.js
@@ -0,0 +1,28 @@
+// Comentário
+/*Grande comentário */
+var nome = "Thiago";
+var idade = 17;
+alert("Bem vindo " + nome + "\nVocê tem " + idade + " anos!");
+
+
+var frase = "Japão é o melhor time do mundo!"
+
+// Log no console, veja o console! Ele é muito importante. Como: Source, log e outros.
+console.log(5+5);
+console.log(nome + idade);
+
+//----- Editando Textos
+
+//usando o replace
+console.log(frase.replace("Japão", "Brasil"));
+
+alert(frase.replace("Japão", "Brasil"));
+
+//Usando o uppercase/lowercase
+var frase2 = "Whatsapp é muito legal";
+console.log(frase2.toUpperCase());
+console.log(frase2.toLowerCase());
+
+//----Comandos de álgebra
+// A álgebra do Javascript é igual a todas as outras linguagens
+
