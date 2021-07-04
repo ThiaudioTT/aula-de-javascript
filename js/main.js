487d2ac74a65d83bc67ce7c3087b1efa42ba8772
Thiago Araújo <thiago.sky25@gmail.com>
2021-07-04 19:57:22 -0300

    continuação
    adicionado algumas coisas a mais

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 // Comentário
 /*Grande comentário */
+
+/* Dica: Você pode usar o F12 (Go to definition) para um melhor entendimento
+da função no VScode */
+
+
+
 var nome = "Thiago";
 var idade = 17;
 alert("Bem vindo " + nome + "\nVocê tem " + idade + " anos!");
@@ -26,3 +32,44 @@ console.log(frase2.toLowerCase());
 //----Comandos de álgebra
 // A álgebra do Javascript é igual a todas as outras linguagens
 
+//----- Listas/Arrays/Matrizes
+
+var lista = ["maçã", "pêra", "whatsapp"];
+
+//consulta
+console.log(lista);
+
+console.log(lista[0]);
+//adicionando
+lista.push("Whatsapp Simulator");
+console.log(lista);
+
+// Tamanho
+console.log("Tamanho da lista: " + lista.length);
+
+// para string, a lista vira uma string com vírgula.
+console.log("Lista para string: " + lista.toString());
+
+// Transformando em String apenas um elemento (character):
+console.log("Apenas um elemento: " + lista.toString()[4]);
+
+//Função Join: Adds all the elements of an array into a string, separated by the specified separator string.
+console.log(lista.join(" - "));
+
+//---- Dicionários
+
+var fruta = {nome:"maçã", cor:"vermelha"};
+console.log("Dicionários: ");
+console.log(fruta);
+
+//consultando um elemento:
+console.log(fruta.cor)
+
+
+//---- Arrays e dicionários:
+
+var frutas_lista = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxo"}];
+console.log("Lista de dicionários: ");
+console.log(frutas_lista);
+//consulta
+console.log(frutas_lista[1].cor);
